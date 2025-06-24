a35cde3e582b7c550bce72cf549892ce53ffc9e5
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-06-24 15:16:45 -0700

    rendered array onto dom, add boxes in the css

--- array.js
+++ array.js
@@ -1,3 +1,15 @@
-const nums =[0, 1, 2, 3, 4, 5]
+console.log('hello')
 
-document.getElementsByClassName("arrayBox").innerHTML = "<h1>Hello</h1>";
+//list of array of numbers
+const nums =[0, 1, 2, 3, 4, 5];
+
+//Normal for loop that prints out nums in console
+for (let i = 0; i < nums.length; i++){
+    console.log(nums[i]);
+}
+
+//Refactor the code to work with the DOM
+const displayNums = document.querySelector("#arrayBox")
+for (let i = 0; i < nums.length; i++){
+    displayNums.innerHTML += `<h1>${nums[i]}</h1>`
+}
